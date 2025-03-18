4b733b8bfebdad3cde22c2b9e069b2636fd72490
Matheus Alves <matheus.jeje2@gmail.com>
2025-03-18 11:17:58 -0300

    ADD: Adicionando botão de excluir e adicionar

--- model/controler_mensagem.py
+++ model/controler_mensagem.py
@@ -1,44 +1,58 @@
-import datetime
 from data.conexao import Conexao
 
 class Mensagem:
-    def cadastrar_mensagem(usuario, mensagem):
-    
-        data_hora = datetime.datetime.today()
-
+    @staticmethod
+    def recuperar_mensagens():
         conexao = Conexao.criar_conexao()
-    
+        cursor = conexao.cursor(dictionary=True)
+
+        cursor.execute("SELECT cod_comentario, nome, comentario, data_hora, curtidas FROM tb_comentarios ORDER BY data_hora DESC")
+        mensagens = cursor.fetchall()
+
+        cursor.close()
+        conexao.close()
+        return mensagens
+
+    @staticmethod
+    def cadastrar_mensagem(nome, comentario):
+        conexao = Conexao.criar_conexao()
         cursor = conexao.cursor()
 
-        sql = "INSERT INTO tb_comentarios(nome,data_hora, comentario) VALUES(%s,%s,%s)"
-
-        valores = (usuario, data_hora, mensagem)
+        sql = "INSERT INTO tb_comentarios (nome, comentario, data_hora) VALUES (%s, %s, NOW())"
+        valores = (nome, comentario)
 
         cursor.execute(sql, valores)
-
         conexao.commit()
 
         cursor.close()
-
         conexao.close()
-        
-    def recuperar_mensagens():
-        
+
+    @staticmethod
+    def excluir_mensagem(cod_comentario):
         conexao = Conexao.criar_conexao()
-        
-        cursor = conexao.cursor(dictionary= True)
-        
-        sql = """SELECT nome as usuario, comentario as mensagem, data_hora FROM tb_comentarios"""
-        
-        cursor.execute(sql)
-        
-        resultado = cursor.fetchall()
-        
-        cursor.close()
-        
-        conexao.close()
-        
-        return resultado
-    
+        cursor = conexao.cursor()
 
-        
+        sql = "DELETE FROM tb_comentarios WHERE cod_comentario = %s"
+        valores = (cod_comentario,)
+
+        cursor.execute(sql, valores)
+        conexao.commit()
+
+        cursor.close()
+        conexao.close()
+
+    @staticmethod
+    def curtir_mensagem(cod_comentario):
+        conexao = Conexao.criar_conexao()
+        cursor = conexao.cursor()
+
+        # Atualiza o número de curtidas
+        sql = "UPDATE tb_comentarios SET curtidas = curtidas + 1 WHERE cod_comentario = %s"
+        valores = (cod_comentario,)
+
+        cursor.execute(sql, valores)
+        conexao.commit()
+
+        cursor.close()
+        conexao.close()
+
